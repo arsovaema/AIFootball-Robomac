f469898b6dcc27502415c512b525a5fb3b52b4b1
Lora <misevskalora@gmail.com>
2026-04-22 16:56:50 +0200

    Update Manager.py

--- src/Test_team/Manager.py
+++ src/Test_team/Manager.py
@@ -1,34 +1,105 @@
-# Choose names for your players and team
-    # Choose a funny name for each player and your team
-    # Use names written only in cyrillic
-    # Make sure that the name is less than 11 characters
-    # Don't use profanity!!!
+import numpy as np
+
+RIGHT_GOAL_X  = 1316
+LEFT_GOAL_X   = 50
+GOAL_TOP      = 343
+GOAL_BOTTOM   = 578
+GOAL_CENTER_Y = (GOAL_TOP + GOAL_BOTTOM) / 2
+GK_MARGIN     = 40
+
 def team_properties():
     properties = dict()
-    player_names = ["Пробалдо", "Панчевалдо", "Елмасалдо"]
-    properties['team_name'] = "Проба UTD"
-    properties['player_names'] = player_names
-    properties['image_name'] = 'Blue.png' # use image resolution 153x153
-    properties['weight_points'] = (20, 15, 15)
-    properties['radius_points'] = (20, 15, 20)
+    properties['team_name'] = "Cutie.py"
+    properties['player_names'] = ["Nate", "Maddie", "Cassie"]
+    properties['image_name'] = 'Blue.png'
+    properties['weight_points']           = (20, 15, 15)
+    properties['radius_points']           = (20, 15, 20)
     properties['max_acceleration_points'] = (40, 10, 15)
-    properties['max_speed_points'] = (40, 15, 25)
-    properties['shot_power_points'] = (30, 25, 18)
+    properties['max_speed_points']        = (40, 15, 25)
+    properties['shot_power_points']       = (30, 25, 18)
     return properties
 
-# This function gathers game information and controls each one of your three players
+def goalkeeper_decision(player, ball, your_side):
+    """Голманот се движи само горе-доле по линијата на голот."""
+    gk_x = LEFT_GOAL_X + GK_MARGIN if your_side == 'left' else RIGHT_GOAL_X - GK_MARGIN
+
+    target_y = np.clip(ball['y'], GOAL_TOP + player['radius'], GOAL_BOTTOM - player['radius'])
+
+    dx = gk_x - player['x']
+    dy = target_y - player['y']
+    dist = np.sqrt(dx**2 + dy**2)
+
+    return {
+        'alpha': np.arctan2(dy, dx),
+        'force': player['a_max'] * player['mass'] if dist > 5 else 0,
+        'shot_request': False,
+        'shot_power': 0
+    }
+
 def decision(our_team, their_team, ball, your_side, half, time_left, our_score, their_score):
+    attack_goal_x = RIGHT_GOAL_X if your_side == 'left' else LEFT_GOAL_X
+    defend_goal_x = LEFT_GOAL_X  if your_side == 'left' else RIGHT_GOAL_X
+
     manager_decision = [dict(), dict(), dict()]
+
     for i in range(3):
-        
-        # python dictionary with the following keys: 'x', 'y', 'alpha', 'mass', 'radius', 'a_max', 'v_max', 'shot_power_max'
-        player = our_team[i] # gather information about each one of your players eg. position
-        manager_decision[i]['alpha'] = player['alpha'] # player['alpha'] # choose direction for running (0, 2*pi)
-        manager_decision[i]['force'] = 0 # accelerate or deaccelerate your player up to 'v_max' or 0: (-0.5 * 'a_max' * 'mass', 'a_max' * 'mass')
-        manager_decision[i]['shot_request'] = True # choose if you want to shoot
-        manager_decision[i]['shot_power'] = 100 # use different shot power: (0, 'shot_power_max')
-    # print(our_score, their_score)
-    # print(our_team[0]['weight'], our_team[0]['radius'], our_team[0]['max_acceleration'], our_team[0]['max_speed'], our_team[0]['shot_power'])
-    # print(their_team[0]['weight'], their_team[0]['radius'], their_team[0]['max_acceleration'], their_team[0]['max_speed'], their_team[0]['shot_power'])
-    
+        player = our_team[i]
+
+        # --- Голман ---
+        if i == 0:
+            manager_decision[i] = goalkeeper_decision(player, ball, your_side)
+            continue
+
+        dist_to_ball = np.sqrt((ball['x'] - player['x'])**2 + (ball['y'] - player['y'])**2)
+        alpha_to_ball = np.arctan2(ball['y'] - player['y'], ball['x'] - player['x'])
+        alpha_to_goal = np.arctan2(GOAL_CENTER_Y - player['y'], attack_goal_x - player['x'])
+        near_ball = dist_to_ball < 80
+
+        # --- Дефендер (i=1): останува во сопствената половина ---
+        if i == 1:
+            mid_x = (LEFT_GOAL_X + RIGHT_GOAL_X) / 2
+
+            # Целна позиција: помеѓу голот и средината, следи Y на топката
+            defend_x = defend_goal_x + (GK_MARGIN + 150) * (1 if your_side == 'left' else -1)
+            defend_y = np.clip(ball['y'], GOAL_TOP, GOAL_BOTTOM)
+
+            dx = defend_x - player['x']
+            dy = defend_y - player['y']
+            dist_to_pos = np.sqrt(dx**2 + dy**2)
+
+            # Ако топката е во сопствената половина – оди кон неа
+            ball_in_own_half = (ball['x'] < mid_x) if your_side == 'left' else (ball['x'] > mid_x)
+
+            if ball_in_own_half and near_ball:
+                manager_decision[i] = {
+                    'alpha': alpha_to_goal,  # удри кон противничкиот гол
+                    'force': player['a_max'] * player['mass'],
+                    'shot_request': True,
+                    'shot_power': player['shot_power_max']
+                }
+            elif ball_in_own_half:
+                manager_decision[i] = {
+                    'alpha': alpha_to_ball,
+                    'force': player['a_max'] * player['mass'],
+                    'shot_request': False,
+                    'shot_power': 0
+                }
+            else:
+                # Топката е во противничката половина – остани на позиција
+                manager_decision[i] = {
+                    'alpha': np.arctan2(dy, dx),
+                    'force': player['a_max'] * player['mass'] if dist_to_pos > 10 else 0,
+                    'shot_request': False,
+                    'shot_power': 0
+                }
+            continue
+
+        # --- Напаѓач (i=2): секогаш притискај ---
+        manager_decision[i] = {
+            'alpha': alpha_to_goal if near_ball else alpha_to_ball,
+            'force': player['a_max'] * player['mass'],
+            'shot_request': near_ball,
+            'shot_power': player['shot_power_max']
+        }
+
     return manager_decision
